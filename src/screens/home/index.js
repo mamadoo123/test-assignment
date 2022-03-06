54e05858dccdeec5e6b94078e76f3e767946d724
Mohamed Khamis <mkhs66662000@gmail.com>
2022-03-06 15:25:46 +0200

    cleaning some codes

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import HomeButton from '../../components/homeButton';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -13,29 +13,59 @@ const HomeScreen = ({navigation}) => {
     style: {marginRight: 8} 
   }
   return (
-    <View style={{flex: 1, alignItems: 'center', paddingTop: Height * 0.05, backgroundColor: colors.white}}>
+    <View style={styles.screen}>
       <HomeButton
         title = "Assets Inventory"
-        iconComponent={ <Ionicons name="md-browsers-outline" {...iconProps} />} 
+        iconComponent={ 
+          <Ionicons 
+            name="md-browsers-outline" 
+            {...iconProps} 
+          />
+        } 
         handler = {() => navigation.navigate('AssetInventory')}
-        otherStyles={{marginBottom: Height * 0.03}}
+        otherStyles={{
+          marginBottom: Height * 0.03
+        }}
       />
 
       <HomeButton
         title = "Model"
-        iconComponent={<Ionicons name="md-bar-chart-outline" {...iconProps} />}
+        iconComponent={
+          <Ionicons 
+            name="md-bar-chart-outline" 
+            {...iconProps} 
+          />
+        }
         handler = {() => navigation.navigate('ModelsList')}
-        otherStyles={{marginBottom: Height * 0.03}}  
+        otherStyles={{
+          marginBottom: Height * 0.03
+        }}  
       />
 
       <HomeButton
         title = "Person" 
-        iconComponent={ <MaterialIcons name="person-search" {...iconProps} />}
+        iconComponent={ 
+          <MaterialIcons 
+            name="person-search" 
+            {...iconProps} 
+          />
+        }
         handler = {() => navigation.navigate('Person')}
-        otherStyles={{marginBottom: Height * 0.03}}
+        otherStyles={{
+          marginBottom: Height * 0.03
+        }}
       />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screen:{
+    flex: 1, 
+    alignItems: 'center', 
+    paddingTop: Height * 0.05, 
+    backgroundColor: colors.white
+  }
+})
 
 export default HomeScreen
