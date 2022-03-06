9e300c82e864ba23220a7fe4c32db99cce6e4796
Mohamed Khamis <mkhs66662000@gmail.com>
2022-03-06 09:34:25 +0200

    bilerplate for screen 3

---
+++ src/components/itemDetails.js/imageInfo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ImageInfo = () => {
+  return (
+    <View>
+      <Text>ImageInfo</Text>
+    </View>
+  )
+}
+
+export default ImageInfo
+
+const styles = StyleSheet.create({})
---
+++ src/components/itemDetails.js/topImage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TopImage = () => {
+  return (
+    <View>
+      <Text>TopImage</Text>
+    </View>
+  )
+}
+
+export default TopImage
+
+const styles = StyleSheet.create({})
